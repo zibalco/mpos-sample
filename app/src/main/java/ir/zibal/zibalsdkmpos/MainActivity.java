e836a3772c9ba6da46a5fef81a4915acbee1ceb2
ghadiri <ghadirinia1373@gmail.com>
2020-07-18 14:58:23 +0430

    refactor for new sdk

--- app/src/main/java/ir/zibal/zibalsdkmpos/MainActivity.java
+++ app/src/main/java/ir/zibal/zibalsdkmpos/MainActivity.java
@@ -1,86 +1,51 @@
 package ir.zibal.zibalsdkmpos;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
-import ir.zibal.zibalsdk.IdToZibalServer;
-import ir.zibal.zibalsdk.ZibalAPI;
+//import ir.zibal.zibalsdk.ZibalAPI;
+//import ir.zibal.zibalsdk.ZibalActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_balance;
-    Button btn_inject_keys;
-    Button btn_bluetooth;
-    Button btn_payment;
-    ZibalAPI zibalAPI;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
 
-        zibalAPI = ZibalAPI.getInstance(this);
-    }
 
-    void initViews() {
-        btn_balance = findViewById(R.id.btn_balance);
-        btn_inject_keys = findViewById(R.id.btn_inject_keys);
-        btn_bluetooth = findViewById(R.id.btn_bluetooth);
-        btn_payment = findViewById(R.id.btn_payment);
-
-        btn_balance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                zibalAPI.getBalance();
-            }
-        });
-
-        btn_inject_keys.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                zibalAPI.injectKeys();
-            }
-        });
-
-        btn_bluetooth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                zibalAPI.navigateToBluetooth(MainActivity.this);
-            }
-        });
-
+        Button btn_payment = findViewById(R.id.payBtn);
+        final EditText et_zibalId = findViewById(R.id.zibalIdEditText);
         btn_payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), InputActivity.class);
-                startActivityForResult(intent, 1001);
+
+                String zibalId = et_zibalId.getText().toString();
+                if(zibalId.length() == 0){
+                    Toast.makeText(MainActivity.this,"لطفا شناسه زیبال را وارد کنید.",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+//                    Intent intent = new Intent(MainActivity.this, ZibalActivity.class);
+//                    intent.putExtra("zibalId",zibalId);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("error happened", e.toString());
+                }
 
 
             }
         });
-
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == 1001) {
-
-            if (resultCode == RESULT_OK) {
-                String zibalId = data.getStringExtra("zibalId");
-                new IdToZibalServer(MainActivity.this, MainActivity.this,zibalId).execute();
-            }
-
-
-        }
-
-
-
-    }
-
 }
+
